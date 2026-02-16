7c74226a130f5eb74f5689f0eb29e99a3ed5b9b0
shubham-gupta-525 <shubhamgupta11101@gmail.com>
2026-02-16 20:56:46 +0530

    Ready for Render deployment

--- ems_main/settings.py
+++ ems_main/settings.py
@@ -32,7 +32,10 @@ SECRET_KEY = 'django-insecure-rl++4_d%&rd%ddme0t#9+#xl88&m6dyz6gizfd$z1jy-5$dh2n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Application definition
@@ -126,13 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_URL = 'static/'
 
 
 import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-ALLOWED_HOSTS = ['shubham525.pythonanywhere.com']
