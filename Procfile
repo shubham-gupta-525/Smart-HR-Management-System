4731c7013a149f634153563dc1946a9523633ee5
shubham-gupta-525 <shubhamgupta11101@gmail.com>
2026-02-18 00:17:38 +0530

    ready for render deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn ems_main.wsgi --bind 0.0.0.0:$PORT
+web: gunicorn ems_main.wsgi 
--- ems_main/settings.py
+++ ems_main/settings.py
@@ -30,11 +30,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-rl++4_d%&rd%ddme0t#9+#xl88&m6dyz6gizfd$z1jy-5$dh2n'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.koyeb.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1', 'localhost']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
